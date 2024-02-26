b571c0555cf9e844ff04310c5ee692d41a634590
김상연 <ksang98@icloud.com>
2024-02-26 21:45:40 +0900

    section2: Header UI 만들기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Fragment } from "react";
+import Header from '../components/common/Header';
 
 export default function Home() {
 
-  const a = {
-    a: 1,
-    b: 1
-  }
   return (
-    <>
-      Hello World!
-    </>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
